fc173729eef90f11a756e88048e791d1a6535291
md shahid <shahiddelhi989@gmail.com>
2025-11-26 12:37:16 +0530

    added new file

--- frontend/java-script/chapter-07(project)/02-mousecoursor/index.js
+++ frontend/java-script/chapter-07(project)/02-mousecoursor/index.js
@@ -7,6 +7,8 @@ const colors=[
   '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D'
 ];
 
+const scale=['scale(1)','scale(2)','scale(1.5)','scale(3)','scale(2.5)']
+
 
 // Move cursor with mouse and change color
 document.addEventListener('mousemove', (event) => {
@@ -15,8 +17,9 @@ document.addEventListener('mousemove', (event) => {
 
   cursor.style.left = x + 'px';
   cursor.style.top = y + 'px';
-
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  cursor.style.backgroundColor = randomColor;
+  const randomcolor=colors[Math.floor(Math.random() * colors.length)]
+  const randomscale = scale[Math.floor(Math.random() * scale.length)];
+  cursor.style.transform = randomscale
+  cursor.style.backgroundColor=randomcolor
 });
 
