4aa35b3ecfe22900893e8a09a8c1bc933eeb6105
md shahid <shahiddelhi989@gmail.com>
2026-01-22 12:30:46 +0530

    added

--- frontend/java-script/chapter-02/1.variables/3.interview_question/let_vs_const_vs_var10.js
+++ frontend/java-script/chapter-02/1.variables/3.interview_question/let_vs_const_vs_var10.js
@@ -13,55 +13,15 @@ console.log(a)
 
 
 //second diffrence declaration without intilization 
-//we can declare the variable first with let and var and then later on we can intilage it but with const we must has to intialize the variable
+//we can declare the variable first with let and var and then later on we can intilage it but with const we must has to intialize the variable and after intilzation we can't reintilze it
 
 
-
-
-//3rd diffrence hoisting
-//function abc() {
-   // let d = 9//hoisted and intiallized
-    //console.log(a)
-    //console.log(b)
-    // console.log(c)
-   // console.log(d)
-
-    //let a=9//hoisted but unitialize(tdz)
-    //var b=8//hoisted and intialized with u ndefined
-    // const c=6//hoisted but unitialized(tdz)
-}
-
-abc()
-
-//4th diffrence 
-// let,const can be local as well as global and block where as var can be only local and global if it is insdie the block then it is treated as outside the block
+//3rd diffrence 
+// let,const can be local , global as well as  block where as var can be only local and global if it is insdie the block then it is treated as outside the block
 
 // so if outside the block there is function then it is treatd as local and if outsite is not any function then it is treated as global
 
 
-//Example:
-// {
-//     var x = 10;
-//     let y = 20;
-//     const z = 30;
-// }
-
-//console.log(x); // ✅ 10 → because `var` is hoisted to global
-//console.log(y); // ❌ ReferenceError → block scoped
-//console.log(z); // ❌ ReferenceError → block scoped
-
-// Another example with a function:
-// function test() {
-//     if (true) {
-//         var a = 1;
-//         let b = 2;
-//         const c = 9;
-//     }
-
-//     console.log(a); // ✅ 1
-//     console.log(b); // ❌ ReferenceError → because `b` is block scoped
-//     console.log(c);
-// }
 
 //Summary:
 //Keyword	     Function Scope	      Block Scope    global scope
