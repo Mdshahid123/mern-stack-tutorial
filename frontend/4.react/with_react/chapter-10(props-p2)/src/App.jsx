e5d3ce6e50ef63ad687f5160bb6cd0d509f42621
md shahid <shahiddelhi989@gmail.com>
2026-05-15 11:00:20 +0530

    added

---
+++ frontend/4.react/with_react/chapter-10(props-p2)/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header"
+import Footer from "./components/footer"
+import "./app.css"
+function App()
+{
+    return(
+      <>
+      <Header></Header>
+      
+      <Footer></Footer>
+
+      </>
+         
+    )
+}
+
+
+export default App
---
+++ frontend/4.react/with_react/chapter-10(props-p2)/src/components/Header.jsx
@@ -0,0 +1,23 @@
+function Header()
+{
+    return(
+        <header className="header">
+
+          <h1 className="logo">Skill Bridge</h1>
+
+          <nav>
+
+            <a href="#">Home</a>
+
+            <a href="#">Mentors</a>
+
+            <a href="#">Founders</a>
+
+          </nav>
+
+      </header>
+    )
+}
+
+
+export default Header
