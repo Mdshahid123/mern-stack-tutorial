47d2d994bf01c39c2babc2b7c2d4945976101208
md shahid <shahiddelhi989@gmail.com>
2026-06-25 10:24:28 +0530

    added

--- gen-ai/project/2.chatgpt-v2/app.js
+++ gen-ai/project/2.chatgpt-v2/app.js
@@ -1,6 +1,7 @@
 
 const express=require("express")
 const userRoutes=require("./router/user")
+const mongoose=require("mongoose")
 const dotenv=require("dotenv")
 dotenv.config()
 
@@ -10,8 +11,21 @@ app.use(express.static("public"))
 app.use(express.json())
 app.use(userRoutes)
 
-const port=process.env.port || 4000
-app.listen(port,()=>{
-   console.log(`server is listning at port:${port}`)
-})
+async function connect_with_mongodb()
+{
+   try {
+          const promise=await mongoose.connect(process.env.mongodb_cloud)
+          console.log("mongodb connected successfully")
+          const port=process.env.port || 4000
+          app.listen(port,()=>{
+             console.log(`server is listning at port:${port}`)
+          })
 
+   } catch (error) {    
+         console.log("mongo db connection failed:",error)
+   }      
+
+}   
+
+
+connect_with_mongodb()
